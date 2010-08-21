c0b1627ae80475eac07d137076f2f87e705cb8fa
Alex Tkachman <alex.tkachman@gmail.com>
2010-08-21 07:59:22 +0300

    groovy.util -> groovypp.util etc.

---
+++ Grailspp/grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,8 @@
+grails.project.dependency.resolution = {
+    repositories {
+        mavenRepo "http://groovypp.artifactoryonline.com/groovypp/libs-releases-local"
+    }
+    dependencies {
+        compile 'org.codehaus.groovy:groovy-all:1.7.5-SNAPSHOT-gpp-#version'
+    }
+}
