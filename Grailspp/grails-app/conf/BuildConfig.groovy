ff4e68dc3e23af4ad443da31ceafebf32bb5962e
Alex Tkachman <alex.tkachman@gmail.com>
2010-08-21 09:56:44 +0300

    Grails plugin without rewriting grails installation

--- Grailspp/grails-app/conf/BuildConfig.groovy
+++ Grailspp/grails-app/conf/BuildConfig.groovy
@@ -1,8 +1,5 @@
 grails.project.dependency.resolution = {
-    repositories {
-        mavenRepo "http://groovypp.artifactoryonline.com/groovypp/libs-releases-local"
-    }
     dependencies {
-        compile 'org.codehaus.groovy:groovy-all:1.7.5-SNAPSHOT-gpp-#version'
+        compile 'org.codehaus.groovy:groovy-all:1.7.5-gpp-#version'
     }
 }
